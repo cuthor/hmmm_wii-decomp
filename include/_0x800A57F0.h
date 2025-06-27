4a75d7fa1d1482cec7f4cc01074b59bc701fe953
cuthor <217993007+cuthor@users.noreply.github.com>
2025-06-26 22:20:36 -0700

    cleaning up struct

--- include/_0x800A57F0.h
+++ include/_0x800A57F0.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include "dolphin/types.h"
+
 typedef struct {
-    unsigned short int _0x0;
-    unsigned char  _gap0x2[0x32];
-    unsigned long  _0x34;
-    unsigned char  _0x38;
-    unsigned long  _0x3C;
+    u16  us_0x0;
+    u8   gap_0x2[0x32];
+    u32  ui_0x34;
+    u8   uc_0x38;
+    u32  ui_0x3C;
 } _0x800A57F0_T;
 
 
